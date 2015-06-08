269ffc8fa11983ae9d793d363a31560da21c0b63
Dawid Samołyk <dawid.samolyk@gmail.com>
2015-06-08 23:38:18 +0200

    Synchronizacja ruchu pomiędzy graczami.

---
+++ Assets/C_PlayerManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class C_PlayerManager : MonoBehaviour
+{
+	private PhotonView view;
+	private Transform cameraPosition;
+
+	void Start ()
+	{
+		view = GetComponent<PhotonView> ();
+	}
+
+	private void Awake ()
+	{
+		cameraPosition = Camera.main.transform;
+	}
+
+	public void Update ()
+	{
+		if (view.isMine) {
+			float horizontalMotion = CrossPlatformInputManager.GetAxis ("Horizontal");
+			float verticalMotion = CrossPlatformInputManager.GetAxis ("Vertical");
+			bool jumpMotion = CrossPlatformInputManager.GetButton ("Jump");
+
+			Vector3 camForward = Vector3.Scale (cameraPosition.forward, new Vector3 (1, 0, 1)).normalized;
+			Vector3 move = (verticalMotion * camForward + horizontalMotion * cameraPosition.right).normalized;
+
+			view.RPC ("updateClientMove", PhotonTargets.All, move, jumpMotion);
+		}
+	}
+}
