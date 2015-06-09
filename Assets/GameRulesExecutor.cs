62eaddccd1c29521860e5f846daef65170cc0f0f
Dawid Samołyk <dawid.samolyk@gmail.com>
2015-06-09 22:46:32 +0200

    Zrzucanie monet i boostów w momencie podłączenia gracza.

---
+++ Assets/GameRulesExecutor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameRulesExecutor : MonoBehaviour {
+	public GameObject extraItemsGenerator;
+	private ExtraItemsGenerator generator;
+	private int lastPlayersCount = 0;
+
+	void Start () {
+		generator = extraItemsGenerator.GetComponent<ExtraItemsGenerator> ();
+	}
+
+	public void OnPhotonPlayerConnected(PhotonPlayer player)
+	{
+		Room room = PhotonNetwork.room;
+
+		if (room != null && room.playerCount > 1) {
+			generator.generateCoins();
+			generator.generateBoosts();
+		}
+	}
+}
